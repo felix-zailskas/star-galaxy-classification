fb23bbb4800046fef1c21cc70777a44d02e1ff0f
Felix Zailskas <felixzailskas@gmail.com>
2023-05-22 15:27:07 +0200

    Data downloader files

---
+++ src/data_acquisition/downloader.sh
@@ -0,0 +1,15 @@
+# Stars and Galaxies
+wget https://data.sdss.org/sas/dr17/eboss/sweeps/dr13_final/301/calibObj-008162-6-gal.fits.gz
+wget https://data.sdss.org/sas/dr17/eboss/sweeps/dr13_final/301/calibObj-008162-6-star.fits.gz
+
+# Make a specification file ... you can fill this with files to download.
+
+echo frame-r-008162-6-0080.fits.bz2 >  spec-list.txt
+echo frame-g-008162-6-0080.fits.bz2 >> spec-list.txt
+echo frame-i-008162-6-0080.fits.bz2 >> spec-list.txt
+echo frame-u-008162-6-0080.fits.bz2 >> spec-list.txt
+echo frame-z-008162-6-0080.fits.bz2 >> spec-list.txt
+echo frame-irg-008162-6-0080.jpg    >> spec-list.txt
+
+# All images for these stars
+wget -i spec-list.txt -r --no-parent -nd -B 'https://data.sdss.org/sas/dr17/eboss/photoObj/frames/301/8162/6/'
