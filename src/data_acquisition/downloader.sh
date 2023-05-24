a3a1a48b11048f80077aa797ea5237f9b212f3cd
Niccolò Parodi <parodiniccolo@gmail.com>
2023-05-24 11:25:03 +0200

    Updated download path to /data folder and moved files

--- src/data_acquisition/downloader.sh
+++ src/data_acquisition/downloader.sh
@@ -1,6 +1,6 @@
 # Stars and Galaxies
-wget https://data.sdss.org/sas/dr17/eboss/sweeps/dr13_final/301/calibObj-008162-6-gal.fits.gz
-wget https://data.sdss.org/sas/dr17/eboss/sweeps/dr13_final/301/calibObj-008162-6-star.fits.gz
+wget https://data.sdss.org/sas/dr17/eboss/sweeps/dr13_final/301/calibObj-008162-6-gal.fits.gz -P ../../data/
+wget https://data.sdss.org/sas/dr17/eboss/sweeps/dr13_final/301/calibObj-008162-6-star.fits.gz -P ../../data/
 
 # Make a specification file ... you can fill this with files to download.
 
@@ -12,4 +12,4 @@ echo frame-z-008162-6-0080.fits.bz2 >> spec-list.txt
 echo frame-irg-008162-6-0080.jpg    >> spec-list.txt
 
 # All images for these stars
-wget -i spec-list.txt -r --no-parent -nd -B 'https://data.sdss.org/sas/dr17/eboss/photoObj/frames/301/8162/6/'
+wget -i spec-list.txt -r --no-parent -nd -B 'https://data.sdss.org/sas/dr17/eboss/photoObj/frames/301/8162/6/' -P ../../data/
